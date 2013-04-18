f4aeff1a478c584d2504eee9aaa58fc4b0e3d2e7
Scott Gilroy <sgilroy@mit.edu>
2013-04-18 13:49:21 -0400

    Disabled loading of settings from EEPROM because there is currently a bug causing strips to not get updated (no LEDs come on) after a reset.

---
+++ renderEffectBlast.ino
@@ -0,0 +1,48 @@
+void renderEffectBlast(byte idx) {
+  if(fxVars[idx][0] == 0) { // Initialize effect?
+    gammaRespondsToForce = false;
+    fxVars[idx][1] = random(maxHue + 1); // Random hue
+    // Frame-to-frame increment (speed) -- may be positive or negative,
+    // but magnitude shouldn't be so small as to be boring.  It's generally
+    // still less than a full pixel per frame, making motion very smooth.
+//    fxVars[idx][3] = 1 + random(720) / numPixels;
+//    fxVars[idx][3] = 1;
+    fxVars[idx][3] = 4;
+    // Reverse direction half the time.
+    if(random(2) == 0) fxVars[idx][3] = -fxVars[idx][3];
+    fxVars[idx][4] = 0; // Current position
+    fxVars[idx][0] = 1; // Effect initialized
+//    fxVars[idx][5] = 15 + random(360); // wave period
+//    fxVars[idx][5] = 30 + random(150); // wave period (width)
+    fxVars[idx][5] = 720 * 4 / numPixels; // wave period (width)
+//    fxVars[idx][5] = 720 * 8 / numPixels; // wave period (width)
+//    fxVars[idx][5] = random(720 * 2 / numPixels, 180); // wave period (width)
+  }
+
+  byte *ptr = &imgData[idx][0];
+  int alpha;
+  int halfPeriod = fxVars[idx][5] / 2;
+  int distance;
+  long color;
+  long hue = pickHue(fxVars[idx][1]);
+  for(long i=0; i<numPixels; i++) {
+    alpha = getPointChaseAlpha(idx, (i + frontOffset) % numPixels, halfPeriod) + getPointChaseAlpha(idx, (numPixels - 1 - i + frontOffset) % numPixels, halfPeriod);
+//    alpha = getPointChaseAlpha(idx, (i + frontOffset) % numPixels, halfPeriod);
+    if (alpha > 255) alpha = 255;
+    
+    // Peaks of sine wave are white, troughs are black, mid-range
+    // values are pure hue (100% saturated).
+
+    color = hsv2rgb(hue, 255, alpha);
+    *ptr++ = color >> 16; *ptr++ = color >> 8; *ptr++ = color;
+  }
+//  fxVars[idx][4] += fxVars[idx][3];
+//  fxVars[idx][4] %= 720;
+
+  // max force = half way around (360 half degrees)
+  if (!slaveMode)
+    fxVars[idx][4] = map(frontFsrStepFraction, 0, fsrStepFractionMax, 0, 360);
+//  fxVars[idx][5] = map(fsrStepFraction, 720 / numPixels, fsrStepFractionMax, 0, 720 * 2);
+}
+
+
---
+++ renderEffectSlide.ino
@@ -0,0 +1,78 @@
+const int subPixels = 16;
+
+void renderEffectSlide(byte idx) {
+  if(fxVars[idx][0] == 0) { // Initialize effect?
+    gammaRespondsToForce = false;
+    fxVars[idx][1] = random(maxHue + 1); // Random hue
+    // Frame-to-frame increment (speed) -- may be positive or negative,
+    // but magnitude shouldn't be so small as to be boring.
+    fxVars[idx][3] = 1;
+    // Reverse direction half the time.
+//    if(random(2) == 0) fxVars[idx][3] = -fxVars[idx][3];
+    fxVars[idx][4] = 0; // Current position
+    fxVars[idx][0] = 1; // Effect initialized
+    fxVars[idx][5] = subPixels * 8; // width
+  }
+
+  int hue = pickHue(fxVars[idx][1]);
+
+  clearImage(idx);
+  
+  int x = fxVars[idx][4];
+  int width = fxVars[idx][5];
+  drawLine(idx, x, width, hue);
+  
+//  fxVars[idx][3] += fxVars[idx][6];
+//  if (abs(fxVars[idx][6]) > 
+  if (!slaveMode)
+    fxVars[idx][3] = fixSin(720 * millis() / 10000 / 3);
+    
+  fxVars[idx][4] += fxVars[idx][3];
+  if (fxVars[idx][4] < 0)
+  {
+    fxVars[idx][4] += numPixels * subPixels;
+  }
+  fxVars[idx][4] %= numPixels * subPixels;
+}
+
+void drawLine(byte idx, int x, int width, int hue)
+{
+  long color, i;
+  byte alpha = 0;
+  
+  byte *ptr;
+  int xp = 0;
+  // draw a line at position x that is width subpixels wide
+  for(long i=x; i<x + width; i++) {
+    alpha++;
+    if ((i + 1) % subPixels == 0)
+    {
+      color = hsv2rgb(hue, 255, alpha * 255 / subPixels);
+      xp = i / subPixels;
+//      ptr = &imgData[idx][xp % numPixels * 3];
+//      *ptr++ = color >> 16; *ptr++ = color >> 8; *ptr++ = color;
+      setPixel(idx, xp % numPixels, color);
+      alpha = 0;
+    }
+  }
+
+  color = hsv2rgb(hue, 255, alpha * 255 / subPixels);
+  setPixel(idx, (xp + 1) % numPixels, color);
+}
+
+void setPixel(byte idx, int xp, long color)
+{
+  if (idx >= 0 && idx < 3 && xp >= 0 && xp < numPixels * 3)
+  {
+    byte *ptr = &imgData[idx][xp * 3];
+    *ptr++ = color >> 16; *ptr++ = color >> 8; *ptr++ = color;
+  }
+}
+
+void clearImage(byte idx)
+{
+  byte *ptr = &imgData[idx][0];
+  for(long i = 0; i < numPixels; i++) {
+    *ptr++ = 0; *ptr++ = 0; *ptr++ = 0;
+  }
+}
